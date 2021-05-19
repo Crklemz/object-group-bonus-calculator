b32864493018259f951d8875bddc977f94704b57
Chris Klemz <crklemz@gmail.com>
2021-05-19 08:10:25 -0500

    removed loop as it was not needed. fixed bonus adjustment for salary greater than 65000 and for id number less than 5. should be good now

--- client.js
+++ client.js
@@ -40,8 +40,6 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log(employees);
-
 function employeeBonus(employeeNumber, annualSalary, reviewRating) {
   let bonusPercent = 0
   let compensation = 0
@@ -60,9 +58,12 @@ function employeeBonus(employeeNumber, annualSalary, reviewRating) {
     //give 0% bonus of their base salary - do nothing
   } //end base bonus additions
 
-  if (employeeNumber.length < 5 && annualSalary <= 65000) {
-    //give an additional 5% of their base salary
-    bonusPercent = bonusPercent - 1;
+  if (employeeNumber.length < 5){
+    bonusPercent +=5
+  } 
+
+  if (annualSalary > 65000) {
+    bonusPercent -= 1;
   }
 
   if (bonusPercent > 13) {
@@ -71,56 +72,13 @@ function employeeBonus(employeeNumber, annualSalary, reviewRating) {
   }
 
   bonusAmount = annualSalary * (bonusPercent / 100);
-  compensation = annualSalary * (1 - (bonusPercent/100));
+
+  compensation = annualSalary * (1 + (bonusPercent/100));
 
   return { bonusPercentage: bonusPercent, totalCompensation: compensation, totalBonus: bonusAmount, }
 }// end employeeBonus
 
-console.log('running employeeBonus function with 1 employees info', employeeBonus('62347', '63500', 4));
-
-function employeeReview(employeeArray) {
-  console.log(employeeArray);
-
-  for (let i = 0; i < employeeArray.length; i++) {
-    employeeBonus(employeeArray) {
-      let employeeObject = employeeArray[i];
-      let name = employeeObject.name;
-      let bonusPercentage = employeeObject.bonusPercentage;
-      let totalCompensation = employeeObject.totalCompensation;
-      let totalBonus = employeeObject.totalBonus;
-    }    
-  return employeeObject;
-  }
-  
-console.log('running employeeReview', employeeReview(employees));
+console.log('running employeeBonus function with 1 employees info --> should receive 6% bonus', employeeBonus('62347', '63500', 4));
+console.log('running employeeBonus function with 1 employees info --> should receive 13% bonus', employeeBonus('6243', '74750', 5));
 
 
-/*
-***************************************************
-
-//make a function to take in array of penguins, and a size
-//return an array of penguins that are cute and smaller than size
-
-function penguinChecker(listOfPenguins, maxHeight) {
-    console.log(listOfPenguins, maxHeight);
-    let matchingPenguins = []; // temporary holding box
-
-    for(let i = 0; i < listOfPenguins.length; i++) {
-        if(listOfPenguins[i].isCute && listOfPenguins[i].size <= maxHeight) {
-            console.log(listOfPenguins[i], 'its cute! and small enough');
-            matchingPenguins.push(listOfPenguins[i]);
-        }
-    }
-    //return holding box after loop is done
-    return matchingPenguins;
-}
-
-const match = penguinChecker(penguins, 0.5)
-
-console.log('all matching penguins', match);
-
-
-
-
-*/
-}
